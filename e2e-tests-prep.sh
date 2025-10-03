2e3bc738de40b31f550640e09b3b40f700c42441
Jacopo Scazzosi <jacoscaz@users.noreply.github.com>
2025-10-03 17:17:41 +0200

    chore: uses docker image from ghcr.io for e2e tests (#24)
    * chore: uses docker image from ghcr.io for e2e tests

--- e2e-tests-prep.sh
+++ e2e-tests-prep.sh
@@ -2,4 +2,4 @@
 
 docker network create bacnet-js
 
-docker run -d --platform linux/amd64 --network bacnet-js --name bacnet-stack-server bacnet-stack-server
+docker run -d --platform linux/amd64 --network bacnet-js --name bacnet-stack-runner ghcr.io/bacnet-js/bacnet-stack-runner:f2894b2a0ede095e1fe19200c0f1cc137c5e0e48
