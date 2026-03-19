65f01ea16268b6fec540efb29e0a8a3a88d89e28
Jacopo Scazzosi <jacopo@scazzosi.com>
2026-03-19 10:51:34 +0100

    chore: fixes concurrency and glob pattern in e2e test runner

--- e2e-tests-run.sh
+++ e2e-tests-run.sh
@@ -5,4 +5,4 @@ docker run \
   --workdir /app \
   --network bacnet-js \
   "node:$(node -e "process.stdout.write(process.version.slice(1,3))")-alpine" \
-  node --enable-source-maps --test dist
+  node --enable-source-maps --test --test-concurrency=1 dist/tests/*.js
